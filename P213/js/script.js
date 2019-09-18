4ba2430e024bbd07173175d4d971e4dcd20874d2
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-09-18 17:31:53 +0400

    Add files via upload

---
+++ P213/js/script.js
@@ -0,0 +1,26 @@
+// var x = 10;
+// var y = 3;
+// x++;
+// x--;
+// x = 5;
+// x **= 3; //x=x%3
+
+// var x = 15;
+// var y = 15;
+// var age = 19;
+
+
+// var result = (age > 18) ? "Xosh gelmisiz" : "Olmaz";
+
+// if (age > 18) {
+//     console.log("Xosh gelmisiz");
+// } else {
+//     console.log("Olmaz");
+// }
+
+// console.log(result);
+
+// var age = 17;
+// var city = "Baki";
+
+// console.log(!(age < 18 && city == "Baki"));
